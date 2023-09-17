e884f6e7a1a045f7fcebf666fba31f1a1c43c1af
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2023-09-17 11:22:18 +0100

    setting up Menu

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,21 +1,66 @@
-import React from "react";
+import { useState } from "react";
+import logo from "../asset/logo.jpg"
+import { Link } from "react-router-dom";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import MenuIcon from "@mui/icons-material/Menu";
+import { styled, useTheme } from "@mui/material/styles";
+
+import "./Home.css";
 
 const Home = () => {
+  const [open, setOpen] = useState(false)
+
+  const handleDrawerOpen = () => {
+    setOpen(true)
+  }
+
+  const handleDrawerClose = () => {
+    setOpen(false)
+  }
+
+  const DrawerHeader = styled("div")(({ theme }) => ({
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end",
+  }));
+
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src="../asset/logo.jpg" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Ali Project
-        </a>
+        <dev className="logo-container">
+          <img src={logo} alt="logo" width="50rem" height="50rem" />
+          <Link
+            className="App-link"
+            to="/"
+            // target="_blank"
+          >
+            AMS APEX
+          </Link>
+        </dev>
+
+        <dev>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            // edge="start"
+            // sx={{ mr: 2, ...(open && { display: "none" }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Drawer variant="persistent" anchor="right" open={open}>
+            <DrawerHeader>
+              <IconButton onClick={handleDrawerClose}>
+                <ChevronRightIcon />
+              </IconButton>
+            </DrawerHeader>
+          </Drawer>
+        </dev>
       </header>
     </div>
   );
