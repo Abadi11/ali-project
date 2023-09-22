b4cfb256cc921bc145a03de365d15f9d63237369
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2023-09-22 19:54:01 +0100

    How we work second page 04 - 09

--- src/component/homeBody/contexts.js
+++ src/component/homeBody/contexts.js
@@ -5,3 +5,15 @@ export const booking = "Our company receives a booking request from the customer
 export const pickup = "Our company arranges for the pickup of the cargo from the customer's location through a third-party logistics provider.";
 
 export const warehousingInspectionLabelling = "At AAC provides a vast spacious warehousing for receiving the goods from its customers and storing it until it's ready for transport to the next destination. Our warehouse is well-maintained, equipped with the necessary equipment and technology to manage the cargo efficiently. Labelling is an important part of the shipping process, as it helps to ensure that the cargo is properly identified and tracked throughout its journey. Each customer will carry its own code to be labelled in all his cargo shipments. Inspection is a critical aspect of the shipping process, as it helps ensure that the cargo is safe to transport and complies with regulatory requirements. Our company takes this process seriously to make sure does not contain any hazardous goods that not allowed to be shipped to its destination.";
+
+export const loading = "The cargo is loaded onto the vessel, either by the port own personnel or by a stevedoring company.";
+
+export const transport = "The shipping company transports the cargo to the port or terminal, ether by truck, rail, r barge, depending on the location and mode of transport.";
+
+export const documentation = "At AAC prepares the necessary documentation for the shipment, including the detailed Packing list, commercial invoice, Bill of loading, and other custons and regulatory documents.";
+
+export const transit = "The vessel sails to the destination port, with the cargo secured in its containers or stowed in the cargo hold.";
+
+export const arrivalAtDestination = "At the destination port, the cargo is unloaded from the vessel and transferred to the terminal or warehouse through our third party partnership.";
+
+export const delivery = "Our trusted partners at destination arranges for the delivery of the cargo to the customer's location, either by its own vehicles or through a third-party logistics provider to all the cities in Saudi Arabia.";
--- src/component/homeBody/howWeWork/HowWeWork.js
+++ src/component/homeBody/howWeWork/HowWeWork.js
@@ -1,18 +1,35 @@
 import React from "react";
 import { Typography, Paper } from "@mui/material";
 import PhotoBetweenBookingAndPickup from "../../../asset/howWeAre/photoBetween1&2.JPG";
+import LoadingBackground from "../../../asset/howWeAre/4loadingBackground.JPG";
+import TransportBackground from "../../../asset/howWeAre/5Transport.JPG";
+import DocumentationBackground from "../../../asset/howWeAre/6Documentation.JPG";
+import TransitBackground from "../../../asset/howWeAre/7Transit.JPG";
+import ArrivalAtDestinationBackground from "../../../asset/howWeAre/8Arrival.JPG";
+import DeliveryBackground from "../../../asset/howWeAre/9Delivery.JPG";
 
 import "./HowWeWork.css";
-import { booking, howWeWork, pickup, warehousingInspectionLabelling } from "../contexts";
+import { arrivalAtDestination, booking, delivery, documentation, howWeWork, loading, pickup, transit, warehousingInspectionLabelling } from "../contexts";
 function HowWeWork() {
   return (
     <div className="how-we-work-container">
-      <Paper elevation={2} sx={{p:3}}>
+      <Paper elevation={2} sx={{ p: 3 }}>
         <div className="how-we-work-title">
-          <Typography variant="h4" sx={{fontWeight: "bold"}}>How We Work</Typography>
+          <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+            How We Work
+          </Typography>
         </div>
         <div className="how-we-work-introduction">
-          <Typography variant="h6" sx={{lineHeight: "24px", textAlign: "justify", wordBreak: "break-word"}}>{howWeWork}</Typography>
+          <Typography
+            variant="h6"
+            sx={{
+              lineHeight: "24px",
+              textAlign: "justify",
+              wordBreak: "break-word",
+            }}
+          >
+            {howWeWork}
+          </Typography>
         </div>
         <div className="how-we-work-booking-pickup-container">
           <div className="booking-01-container">
@@ -24,11 +41,7 @@ function HowWeWork() {
               >
                 Booking
               </Typography>
-              <Typography
-                variant="body"
-              >
-                {booking}
-              </Typography>
+              <Typography variant="body">{booking}</Typography>
             </div>
           </div>
 
@@ -60,6 +73,153 @@ function HowWeWork() {
             <Typography variant="body">
               {warehousingInspectionLabelling}
             </Typography>
+          </div>
+        </div>
+        {/* The second page of How we work */}
+        <div className="loading-transport-documentation-transit-arrivalAtDestination-delivery-container">
+          <div className="card-container-template">
+            <div className="how-we-work-card-container">
+              <Typography
+                variant="h5"
+                align="center"
+                sx={{ fontWeight: "bold", position: "absolute", m: 1 }}
+              >
+                Loading
+              </Typography>
+              <img
+                src={LoadingBackground}
+                alt="loading-background"
+                width={"99%"}
+              />
+              <Typography variant="body" p={1.5}>
+                {loading}
+              </Typography>
+            </div>
+          </div>
+          {/* 05 - Transport */}
+          <div className="card-container-template">
+            <div className="how-we-work-card-container">
+              <Typography
+                variant="h5"
+                align="center"
+                sx={{ fontWeight: "bold", position: "absolute", m: 1 }}
+              >
+                Transport
+              </Typography>
+              <img
+                src={TransportBackground}
+                alt="transport-background"
+                width={"99%"}
+              />
+              <Typography variant="body" p={1.5}>
+                {transit}
+              </Typography>
+            </div>
+          </div>
+          {/* 06 - Documentation */}
+          <div className="card-container-template">
+            <div className="how-we-work-card-container">
+              <Typography
+                variant="h5"
+                align="center"
+                sx={{
+                  fontWeight: "bold",
+                  position: "absolute",
+                  ml: 5,
+                  mt: 0.8,
+                }}
+              >
+                Documentation
+              </Typography>
+              <img
+                src={DocumentationBackground}
+                alt="documentation-background"
+                width={"99%"}
+                style={{ paddingTop: "5px" }}
+              />
+              <Typography variant="body" p={1.5}>
+                {documentation}
+              </Typography>
+            </div>
+          </div>
+          {/* 07 - Transit */}
+          <div className="card-container-template">
+            <div className="how-we-work-card-container">
+              <Typography
+                variant="h5"
+                align="center"
+                sx={{
+                  fontWeight: "bold",
+                  position: "absolute",
+                  ml: 5,
+                  mt: 5,
+                }}
+              >
+                Transit
+              </Typography>
+              <img
+                src={TransitBackground}
+                alt="transit-background"
+                width={"99%"}
+                style={{ paddingTop: "2rem" }}
+              />
+              <Typography variant="body" p={1.5}>
+                {transit}
+              </Typography>
+            </div>
+          </div>
+          {/* 08 - Arrival At Destination */}
+          <div className="card-container-template">
+            <div className="how-we-work-card-container">
+              <Typography
+                variant="h5"
+                align="center"
+                sx={{
+                  fontWeight: "bold",
+                  position: "absolute",
+                  ml: 5,
+                  mt: 0.8,
+                  width: "10%",
+                }}
+              >
+                Arrival At Destination
+              </Typography>
+              <img
+                src={ArrivalAtDestinationBackground}
+                alt="arrival-at-destination-background"
+                width={"99%"}
+                style={{ paddingTop: "2.5rem" }}
+              />
+              <Typography variant="body" p={1.5}>
+                {arrivalAtDestination}
+              </Typography>
+            </div>
+          </div>
+          {/* 09 - Delivery */}
+          <div className="card-container-template">
+            <div className="how-we-work-card-container">
+              <Typography
+                variant="h5"
+                align="center"
+                sx={{
+                  fontWeight: "bold",
+                  position: "absolute",
+                  ml: 5,
+                  mt: 4.5,
+                }}
+              >
+                Delivery
+              </Typography>
+              <img
+                src={DeliveryBackground}
+                alt="delivery-background"
+                width={"99%"}
+                style={{ paddingTop: "2rem" }}
+              />
+              <Typography variant="body" p={1.5}>
+                {delivery}
+              </Typography>
+            </div>
           </div>
         </div>
       </Paper>
