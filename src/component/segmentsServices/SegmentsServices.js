ef841ada5362f14bd0c062efac95fa15b34a19bf
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2023-09-23 23:03:28 +0100

    Segments and services header

--- src/component/segmentsServices/SegmentsServices.js
+++ src/component/segmentsServices/SegmentsServices.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Typography, Paper } from "@mui/material";
+import SegmentsServicesPhoto from "../../asset/segmentsServices/segmentsServicesHome.JPG"
 
+import "./SegmentsServices.css"
 function SegmentsServices() {
   return (
-    <div>SegmentsServices</div>
-  )
+    <div className="segments-services-component">
+      <div className="segments-services-header-container">
+        <Typography variant="h3">Segments & Services</Typography>
+        <img
+          src={SegmentsServicesPhoto}
+          alt="segments-services-home"
+          width={"40%"}
+        />
+      </div>
+      <div className="segments-services-body-container">
+        <Paper elevation={2} sx={{ p: 3 }}></Paper>
+      </div>
+    </div>
+  );
 }
 
 export default SegmentsServices
