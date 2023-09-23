07f501c98bc5ee4cd489634887ae108a8787be3a
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2023-09-24 00:11:59 +0100

    Finishing local transportation

--- src/component/segmentsServices/SegmentsServices.js
+++ src/component/segmentsServices/SegmentsServices.js
@@ -5,9 +5,10 @@ import mainPointsLogo from "../../asset/main-points-logo.JPG";
 import SegmentsServicesPhoto from "../../asset/segmentsServices/segmentsServicesHome.JPG"
 import SeaShipping from "../../asset/segmentsServices/seaShipping.JPG";
 import AirShipment from "../../asset/segmentsServices/airShipment.JPG";
+import LocalTransportation from "../../asset/segmentsServices/localTransportation.JPG";
 
 import "./SegmentsServices.css"
-import { airShipment, seaShipping } from './constants';
+import { airShipment, localTransportation, seaShipping } from './constants';
 function SegmentsServices() {
   return (
     <div className="segments-services-component">
@@ -53,6 +54,22 @@ function SegmentsServices() {
             <img
               src={AirShipment}
               alt="air-shipment-background"
+              width={"99%"}
+            />
+          </div>
+
+          {/* Local Transportation */}
+          <div className="ss-card-container-local-transportation">
+            <div className="ss-card-container-body-local-transportation">
+              <Typography variant="h5">Local Transportation</Typography>
+              <div className="ss-card-container-body-description">
+                <img src={mainPointsLogo} alt="main-point-logo" width="20rem" />
+                <Typography variant="body">{localTransportation}</Typography>
+              </div>
+            </div>
+            <img
+              src={LocalTransportation}
+              alt="local-transportation-background"
               width={"99%"}
             />
           </div>
