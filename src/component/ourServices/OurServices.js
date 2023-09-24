4ba48a6e8b0ba380328b79fec2ff5142667cf6b6
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2023-09-24 11:56:04 +0100

    Finishing our Services

--- src/component/ourServices/OurServices.js
+++ src/component/ourServices/OurServices.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import OurServicesPhoto from "../../asset/ourServices/ourServices.JPG";
 
+import "./OurServices.css"
 function OurServices() {
   return (
-    <div>OurServices</div>
-  )
+    <div className="our-services-component">
+      <img src={OurServicesPhoto} alt="our-services" width="70%" />
+    </div>
+  );
 }
 
 export default OurServices
